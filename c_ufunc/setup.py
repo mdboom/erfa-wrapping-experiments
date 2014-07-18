b502a7f2509abb687f6f5dc34a1522091e516bfc
Michael Droettboom <mdboom@gmail.com>
2014-07-18 08:59:16 -0400

    Use numpy include path.

--- c_ufunc/setup.py
+++ c_ufunc/setup.py
@@ -1,10 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
+
+import numpy as np
+
+try:
+    numpy_include = np.get_include()
+except AttributeError:
+    numpy_include = np.get_numpy_include()
+
 setup(name='c_ufunc',
       ext_modules=[
           Extension(
               'c_ufunc',
               ['c_ufunc.c'],
-              libraries=['erfa']
+              libraries=['erfa'],
+              include_dirs=[numpy_include]
           )],
       )
